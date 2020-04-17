68d73099f2a4cee5d2cecca85321ce8b167bc49e
Noel Lopes <noel@ipg.pt>
2020-04-17 10:41:05 +0100

    gerar número aleatório

--- app/src/main/java/pt/ipg/adivinha/MainActivity.java
+++ app/src/main/java/pt/ipg/adivinha/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private Random random = new Random();
+    private int numeroAdivinhar = random.nextInt(10) + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
