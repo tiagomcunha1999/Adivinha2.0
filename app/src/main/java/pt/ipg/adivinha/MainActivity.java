04efded0bd5542088c612cde6057bf93a3043629
Tiago Cunha <1700597@sal.ipg.pt>
2020-04-21 18:45:36 +0100

    Ler numero inserido

--- app/src/main/java/pt/ipg/adivinha/MainActivity.java
+++ app/src/main/java/pt/ipg/adivinha/MainActivity.java
@@ -2,7 +2,10 @@ package pt.ipg.adivinha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.Random;
 
@@ -13,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void adivinha(View view) {
+        Intent intent = new Intent(this, NumerosAleatorios.class);
+
+        EditText editTextNumero = (EditText) findViewById(R.id.editTextNumero);
+
+        String strNumero = editTextNumero.getText().toString();
+        int numero;
+        try{
+            int numero = Integer.parseInt(strNumero);
+        } catch (NumberFormatException e){
+            editTextNumero.setError("Numero Invalido");
+            editTextNumero.requestFocus();
+            return;
+        }
+
+        if (numero < 1 || numero > 10) {
+            editTextNumero.setError("O numero que estou a pensar é entre 1 e 10");
+            editTextNumero.requestFocus();
+            return;
+        }
+
+        startActivity(intent);
     }
 }
